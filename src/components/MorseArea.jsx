2102a8b104aec60c89e857e6ef46cd9e16b8c1a5
Saminu Mailafiiia <saminu@Saminus-Laptop.local>
2024-06-10 16:38:17 +0300

    tailwind initial set-up

--- src/components/MorseArea.jsx
+++ src/components/MorseArea.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const MorseArea = () => {
-  return <div></div>
+  return (
+    <div className="text-[20px] font-bold underline cursor-pointer">
+      here works
+    </div>
+  )
 }
 
 export default MorseArea
