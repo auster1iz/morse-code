5ebee9bb3f558ac8e6a934b3765ecef147976014
Saminu Mailafiiia <saminu@Saminus-Laptop.local>
2024-06-10 18:40:11 +0300

    refactored code

--- src/components/MorseArea.jsx
+++ src/components/MorseArea.jsx
@@ -1,61 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react'
-import {
-  FIVE_HUNDRED_MILLISECONDS,
-  TWO_HUNDRED_MILLISECONDS,
-} from '../constants/time'
-import {
-  MORSE_ALPHABET,
-  MORSE_DOT,
-  MORSE_LINE,
-} from '../constants/morse-alphabet'
+import React from 'react'
+import { useMorseCode } from '../hooks/useMorseCode'
 
 const MorseArea = () => {
-  const timer = useRef()
-
-  const [isPressed, setIsPressed] = useState(false)
-  const [pressStartValue, setPressStartValue] = useState(0)
-  const [morseCurrentState, setMorseCurrentState] = useState('')
-  const [text, setText] = useState('')
-
-  const onMouseDown = () => {
-    clearTimeout(timer.current)
-    setIsPressed(true)
-    setPressStartValue(Date.now())
-  }
-  const onMouseUp = () => {
-    setIsPressed(false)
-    const timeDifference = Date.now() - pressStartValue
-    if (timeDifference < TWO_HUNDRED_MILLISECONDS) {
-      setMorseCurrentState((prev) => prev + MORSE_DOT)
-    } else {
-      setMorseCurrentState((prev) => prev + MORSE_LINE)
-    }
-
-    setPressStartValue(0)
-  }
-  const clearText = () => {
-    setText('')
-    setMorseCurrentState('')
-  }
-
-  useEffect(() => {
-    clearTimeout(timer.current)
-    timer.current = setTimeout(() => {
-      const currentLetter = MORSE_ALPHABET[morseCurrentState]
-
-      if (currentLetter) {
-        setText((prev) => prev + currentLetter)
-      }
-
-      setMorseCurrentState('')
-    }, FIVE_HUNDRED_MILLISECONDS)
-  }, [morseCurrentState])
+  const {
+    isPressed,
+    translatedText,
+    morseCurrentState,
+    onMouseDown,
+    onMouseUp,
+    clearText,
+  } = useMorseCode()
 
   return (
     <div className="w-full h-[100vh] flex justify-center items-center bg-black text-white">
       <div className="flex flex-col items-center">
         <div className="w-[300px] h-[54px] mb-8 border-b-2 border-white border-solid text-[32px]">
-          {text} {morseCurrentState}
+          {translatedText} {morseCurrentState}
         </div>
 
         <div
---
+++ src/hooks/useMorseCode.js
@@ -0,0 +1,64 @@
+import { useEffect, useRef, useState } from 'react'
+import {
+  FIVE_HUNDRED_MILLISECONDS,
+  TWO_HUNDRED_MILLISECONDS,
+} from '../constants/time'
+import {
+  MORSE_ALPHABET,
+  MORSE_DOT,
+  MORSE_LINE,
+} from '../constants/morse-alphabet'
+
+export function useMorseCode() {
+  const timer = useRef()
+
+  const [isPressed, setIsPressed] = useState(false)
+  const [pressStartTime, setPressStartTime] = useState(0)
+  const [morseCurrentState, setMorseCurrentState] = useState('')
+  const [translatedText, setTranslatedText] = useState('')
+
+  const onMouseDown = () => {
+    clearTimeout(timer.current)
+    setIsPressed(true)
+    setPressStartTime(Date.now())
+  }
+
+  const onMouseUp = () => {
+    setIsPressed(false)
+
+    const timeDifference = Date.now() - pressStartTime
+
+    if (timeDifference < TWO_HUNDRED_MILLISECONDS) {
+      setMorseCurrentState((prev) => prev + MORSE_DOT)
+    } else {
+      setMorseCurrentState((prev) => prev + MORSE_LINE)
+    }
+  }
+
+  const clearText = () => {
+    setTranslatedText('')
+    setMorseCurrentState('')
+  }
+
+  useEffect(() => {
+    clearTimeout(timer.current)
+    timer.current = setTimeout(() => {
+      const translatedLetter = MORSE_ALPHABET[morseCurrentState]
+
+      if (translatedLetter) {
+        setTranslatedText((prev) => prev + translatedLetter)
+      }
+
+      setMorseCurrentState('')
+    }, FIVE_HUNDRED_MILLISECONDS)
+  }, [morseCurrentState])
+
+  return {
+    isPressed,
+    translatedText,
+    morseCurrentState,
+    onMouseDown,
+    onMouseUp,
+    clearText,
+  }
+}
